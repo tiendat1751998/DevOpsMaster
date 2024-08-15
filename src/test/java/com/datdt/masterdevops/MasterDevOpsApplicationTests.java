54c2dce2f5693bbec51bb189bf0310257f64621a
datdev <Dotiendat1751998@gmail.com>
2024-08-15 16:25:51 +0700

    first commit

---
+++ src/test/java/com/datdt/masterdevops/MasterDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datdt.masterdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterDevOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
